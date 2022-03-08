deb9d69ed224b981636c7a9b7155490cc8bb22fc
Abbas Sultan <abb.sultan@gmail.com>
2022-03-08 04:02:46 +0200

    fix import

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react/cjs/react.development'
+import { useState, useEffect } from 'react'
 import axios from "../axios";
 import "./Row.css";
 
